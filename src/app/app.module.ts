e9c6983f8ec23db6dbb3a2e49956c98c1e899e7d
Ernesto Bonet Moncada <bernestoalberto@gmail.com>
2018-11-16 13:06:45 -0500

    Starting my ng BTC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WalletComponent } from './wallet/wallet.component';
+import { MarketComponent } from './market/market.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WalletComponent,
+    MarketComponent
   ],
   imports: [
     BrowserModule
